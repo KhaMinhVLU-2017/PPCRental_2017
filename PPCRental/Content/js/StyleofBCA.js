6e5e3daf2aa1e0e51178c147450bac606aad8440
NguyenHoangAnh27897 <hoanganh27897@gmail.com>
2017-11-11 16:41:29 +0700

    Update v0.2

---
+++ PPCRental/Content/js/StyleofBCA.js
@@ -0,0 +1,13 @@
+﻿function test() {
+    var cu = document.getElementById('longlon');
+    cu.style.width = '500px';
+    cu.style.height = '500px';
+    cu.style.transition = '2s';
+}
+
+function test2() {
+    var cu = document.getElementById('longlon');
+    cu.style.width = '100px';
+    cu.style.height = ('100px');
+    cu.style.transition = '2s';
+}

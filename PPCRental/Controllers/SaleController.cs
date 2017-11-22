0f55ff17cb542b8764f493f95308082add67b64f
NguyenHoangAnh27897 <hoanganh27897@gmail.com>
2017-11-22 20:39:39 +0700

    Update sale backend v 0.9

--- PPCRental/Controllers/SaleController.cs
+++ PPCRental/Controllers/SaleController.cs
@@ -1,18 +1,159 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PPCRental.Models;
+using PagedList;
 
 namespace PPCRental.Controllers
 {
     public class SaleController : Controller
     {
-        //
+        private PPCRentalEntities db = new PPCRentalEntities();
+
         // GET: /Sale/
-        public ActionResult Index()
+        public ActionResult Index(int? page = 1)
         {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            var properties = db.PROPERTies.ToList();
+            return View(properties.ToPagedList(pageNumber, pageSize)); ;
+        }
+
+        // GET: /Sale/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROPERTY property = db.PROPERTies.Find(id);
+            if (property == null)
+            {
+                return HttpNotFound();
+            }
+            return View(property);
+        }
+
+        // GET: /Sale/Create
+        public ActionResult Create()
+        {
+            ViewBag.District_ID = new SelectList(db.DISTRICTs, "ID", "DistrictName");
+            ViewBag.Status_ID = new SelectList(db.PROJECT_STATUS, "ID", "Status_Name");
+            ViewBag.PropertyType_ID = new SelectList(db.PROPERTY_TYPE, "ID", "CodeType");
+            ViewBag.Street_ID = new SelectList(db.STREETs, "ID", "StreetName");
+            ViewBag.UserID = new SelectList(db.USERs, "ID", "Email");
+            ViewBag.Sale_ID = new SelectList(db.USERs, "ID", "Email");
+            ViewBag.Ward_ID = new SelectList(db.WARDs, "ID", "WardName");
             return View();
         }
-	}
+
+        // POST: /Sale/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,PropertyName,Avatar,Images,PropertyType_ID,Content,Street_ID,Ward_ID,District_ID,Price,UnitPrice,Area,BedRoom,BathRoom,PackingPlace,UserID,Created_at,Create_post,Status_ID,Note,Updated_at,Sale_ID")] PROPERTY property)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PROPERTies.Add(property);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.District_ID = new SelectList(db.DISTRICTs, "ID", "DistrictName", property.District_ID);
+            ViewBag.Status_ID = new SelectList(db.PROJECT_STATUS, "ID", "Status_Name", property.Status_ID);
+            ViewBag.PropertyType_ID = new SelectList(db.PROPERTY_TYPE, "ID", "CodeType", property.PropertyType_ID);
+            ViewBag.Street_ID = new SelectList(db.STREETs, "ID", "StreetName", property.Street_ID);
+            ViewBag.UserID = new SelectList(db.USERs, "ID", "Email", property.UserID);
+            ViewBag.Sale_ID = new SelectList(db.USERs, "ID", "Email", property.Sale_ID);
+            ViewBag.Ward_ID = new SelectList(db.WARDs, "ID", "WardName", property.Ward_ID);
+            return View(property);
+        }
+
+        // GET: /Sale/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROPERTY property = db.PROPERTies.Find(id);
+            if (property == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.District_ID = new SelectList(db.DISTRICTs, "ID", "DistrictName", property.District_ID);
+            ViewBag.Status_ID = new SelectList(db.PROJECT_STATUS, "ID", "Status_Name", property.Status_ID);
+            ViewBag.PropertyType_ID = new SelectList(db.PROPERTY_TYPE, "ID", "CodeType", property.PropertyType_ID);
+            ViewBag.Street_ID = new SelectList(db.STREETs, "ID", "StreetName", property.Street_ID);
+            ViewBag.UserID = new SelectList(db.USERs, "ID", "Email", property.UserID);
+            ViewBag.Sale_ID = new SelectList(db.USERs, "ID", "Email", property.Sale_ID);
+            ViewBag.Ward_ID = new SelectList(db.WARDs, "ID", "WardName", property.Ward_ID);
+            return View(property);
+        }
+
+        // POST: /Sale/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,PropertyName,Avatar,Images,PropertyType_ID,Content,Street_ID,Ward_ID,District_ID,Price,UnitPrice,Area,BedRoom,BathRoom,PackingPlace,UserID,Created_at,Create_post,Status_ID,Note,Updated_at,Sale_ID")] PROPERTY property)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(property).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.District_ID = new SelectList(db.DISTRICTs, "ID", "DistrictName", property.District_ID);
+            ViewBag.Status_ID = new SelectList(db.PROJECT_STATUS, "ID", "Status_Name", property.Status_ID);
+            ViewBag.PropertyType_ID = new SelectList(db.PROPERTY_TYPE, "ID", "CodeType", property.PropertyType_ID);
+            ViewBag.Street_ID = new SelectList(db.STREETs, "ID", "StreetName", property.Street_ID);
+            ViewBag.UserID = new SelectList(db.USERs, "ID", "Email", property.UserID);
+            ViewBag.Sale_ID = new SelectList(db.USERs, "ID", "Email", property.Sale_ID);
+            ViewBag.Ward_ID = new SelectList(db.WARDs, "ID", "WardName", property.Ward_ID);
+            return View(property);
+        }
+
+        // GET: /Sale/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROPERTY property = db.PROPERTies.Find(id);
+            if (property == null)
+            {
+                return HttpNotFound();
+            }
+            return View(property);
+        }
+
+        // POST: /Sale/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PROPERTY property = db.PROPERTies.Find(id);
+            db.PROPERTies.Remove(property);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }

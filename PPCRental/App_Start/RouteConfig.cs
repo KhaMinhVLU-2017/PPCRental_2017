50703cc9053d9c909ae4340f2021ff3044bfb6d5
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2017-11-30 22:25:17 +0700

    Remove SEO URL

--- PPCRental/App_Start/RouteConfig.cs
+++ PPCRental/App_Start/RouteConfig.cs
@@ -15,41 +15,41 @@ namespace PPCRental
 
 
 
-            routes.MapRoute(
-           name: "Trang chu",
-           url: "Trang-chu",
-           defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-           );
+      //      routes.MapRoute(
+      //     name: "Trang chu",
+      //     url: "Trang-chu",
+      //     defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      //     );
 
           
-            routes.MapRoute(
-             name: "About Introduction",
-             url: "Trang-chu/gioi-thieu",
-             defaults: new { controller = "Home", action = "Aboutus", id = UrlParameter.Optional }
-             );
+      //      routes.MapRoute(
+      //       name: "About Introduction",
+      //       url: "Trang-chu/gioi-thieu",
+      //       defaults: new { controller = "Home", action = "Aboutus", id = UrlParameter.Optional }
+      //       );
 
-            routes.MapRoute(
-                name: "Account",
-                url: "Tai-khoan/dang-nhap",
-                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
-            );
-            routes.MapRoute(
-             name: "Fillter loai bat dong san",
-              url: "{Timkiem}/{loaibatdongsan}/{type}-{name}",
-                 defaults: new { controller = "Home", action = "SearchCodeType", id = UrlParameter.Optional },
-                 namespaces: new[] { "HomeController" }
-                 );
+      //      routes.MapRoute(
+      //          name: "Account",
+      //          url: "Tai-khoan/dang-nhap",
+      //          defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
+      //      );
+      //      routes.MapRoute(
+      //       name: "Fillter loai bat dong san",
+      //        url: "{Timkiem}/{loaibatdongsan}/{type}-{name}",
+      //           defaults: new { controller = "Home", action = "SearchCodeType", id = UrlParameter.Optional },
+      //           namespaces: new[] { "HomeController" }
+      //           );
 
-            routes.MapRoute(
-               name: "Search District",
-               url: "Tim-kiem/Quan-{districtid}-{name}",
-               defaults: new { controller = "Home", action = "SearchDistrict", id = UrlParameter.Optional }
-               );
-            routes.MapRoute(
-          name: "Product Detail",
-          url: "{NameController}/{metatitle}-{id}",
-          defaults: new { controller = "Home", action = "Detail", id = UrlParameter.Optional }
-      );
+      //      routes.MapRoute(
+      //         name: "Search District",
+      //         url: "Tim-kiem/Quan-{districtid}-{name}",
+      //         defaults: new { controller = "Home", action = "SearchDistrict", id = UrlParameter.Optional }
+      //         );
+      //      routes.MapRoute(
+      //    name: "Product Detail",
+      //    url: "{NameController}/{metatitle}-{id}",
+      //    defaults: new { controller = "Home", action = "Detail", id = UrlParameter.Optional }
+      //);
             routes.MapRoute(
              name: "Default",
              url: "{controller}/{action}/{id}",

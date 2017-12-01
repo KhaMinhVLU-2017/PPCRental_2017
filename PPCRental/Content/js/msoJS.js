9eb5b4e35fb30f6ac690ee803937134ee905aae1
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2017-12-01 13:13:22 +0700

    Complete interface changepassword

--- PPCRental/Content/js/msoJS.js
+++ PPCRental/Content/js/msoJS.js
@@ -17,9 +17,31 @@ function ChangBGout(){
 	body.style.size='cover';
 	body.style.transition='1s';
 }
+var pwMeo = document.getElementById('pwMeo');
 var pass1 = document.getElementById('passNew1');
-pass1.addEventListener("input",GetPass);
-function GetPass(){
-	var sosanh= pass1.value;
-	console.log(sosanh);
+pass1.addEventListener("input", GetPass);
+var sosanh = "";
+var meo = "";
+function GetPass() {
+    sosanh = pass1.value;
+    if (sosanh != "" && sosanh == meo) {
+        pwMeo.innerText = "";
+    } else if (sosanh != "" && sosanh != meo && meo.length > 0) {
+        pwMeo.style.color = 'red';
+        pwMeo.style.textShadow ='1px 1px 1px white';
+        pwMeo.innerText = "Password and Password Confirm Differnt";
+    }
+}
+var pass2 = document.getElementById('passNew2');
+pass2.addEventListener("input", GetPass2);
+
+function GetPass2() {
+    meo = pass2.value;
+    if (meo == sosanh && meo != "") {
+        pwMeo.innerText = "";
+    } else if (sosanh != "" && sosanh != meo) {
+        pwMeo.style.color = 'red';
+        pwMeo.style.textShadow = '1px 1px 1px white';
+        pwMeo.innerText = "Password and Password Confirm Differnt";
+    }
 }

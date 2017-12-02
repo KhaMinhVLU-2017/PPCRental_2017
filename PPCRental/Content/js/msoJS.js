fb4011506e508747234f98d647f9a9592e384049
TranAnhPhuongbl1997 <trananhphuongbl112@gmail.com>
2017-12-02 17:47:56 +0700

    fix giao dien change password

--- PPCRental/Content/js/msoJS.js
+++ PPCRental/Content/js/msoJS.js
@@ -1,22 +1,5 @@
 var first = document.getElementById('first');
 first.addEventListener("mouseover",ChangBG);
-function ChangBG(){
-	var body = document.getElementsByTagName('body')[0];
-	body.style.backgroundImage='url("/Content/img/img_changepass/bg3.jpg")';
-	body.style.height='100vh';
-	body.style.backgroundPosition='center';
-	body.style.size='cover';
-	body.style.transition='1s';
-}
-first.addEventListener("mouseout",ChangBGout);
-function ChangBGout(){
-	var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage ='url("/Content/img/img_changepass/bg.jpg")';
-	body.style.height='100vh';
-	body.style.backgroundPosition='center'; 
-	body.style.size='cover';
-	body.style.transition='1s';
-}
 var pwMeo = document.getElementById('pwMeo');
 var pass1 = document.getElementById('passNew1');
 pass1.addEventListener("input", GetPass);

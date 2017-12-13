d6cda516eff6b5c26ab53379a42945e325b6be82
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2017-12-13 23:22:03 +0700

    Push test

---
+++ PPCRental.Appceptance.Test/Driver/DetailProjectDriver.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Linq;
+using TechTalk.SpecFlow;
+using PPCRental.Models;
+using PPCRental.Controllers;
+using System.Web.Mvc;
+using PPCRental.Appceptance.Test.Support;
+using FluentAssertions;
+
+
+namespace PPCRental.Appceptance.Test.Driver
+{
+    public class DetailProjectDriver
+    {
+        private readonly CatalogContext _context;
+        private ActionResult _result;
+
+        public DetailProjectDriver(CatalogContext context)
+        {
+            _context = context;
+        }
+
+        public void InsertProjectToDB(Table project)
+        {
+            using(var db = new PPCRentalEntities())
+            {
+                foreach(var item in project.Rows)
+                {
+                    var PropertyPro = new PROPERTY
+                    {
+                        PropertyName = item["PropertyName"],
+                        Avatar = item["Avatar"],
+                        Images = item["Images"],
+                        Content = item["Content"],
+                        Price = int.Parse(item["Price"]),
+                        UnitPrice = item["UnitPrice"],
+                        UserID = int.Parse(item["UserID"])
+                    };
+                    db.PROPERTies.Add(PropertyPro);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        public void ShowPropertyDetail(Table showDetail)
+        {
+            var ProjectDetails = showDetail.Rows.Single();
+
+            var actualProjectDetails = _result.Model<PROPERTY>();
+
+            actualProjectDetails.Should().Match<PROPERTY>(
+              b => b.PropertyName == ProjectDetails["PropertyName"]
+              && b.Content == ProjectDetails["Content"]
+              && b.Price == decimal.Parse(ProjectDetails["Price"]));
+        }
+
+        public void getProject(string idProject)
+        {
+            var Property = _context.ReferenceProperty.GetById(idProject);
+            using (var _controller = new HomeController())
+            {
+                _result = _controller.Detail(int.Parse(idProject));
+            }
+
+        }
+    }
+}
---
+++ PPCRental.Appceptance.Test/Support/ListProperty.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using PPCRental.Models;
+using FluentAssertions;
+
+namespace PPCRental.Appceptance.Test.Support
+{
+    public class ListProperty : Dictionary<string,PROPERTY>
+    {
+        public PROPERTY GetById(string bookId)
+        {
+            return this[bookId.Trim()].Should().NotBeNull()
+                                      .And.Subject.Should().BeOfType<PROPERTY>().Which;
+        }
+    }
+}

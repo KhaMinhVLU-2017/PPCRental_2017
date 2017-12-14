e3bb2f77801a4945dad7e67164c0f4f1288ce0f4
Tranminhchuong <chuongtran04188@gmail.com>
2017-12-14 21:04:50 +0700

    Test US01_FilterProject

--- PPCRental.AcceptanceTest.Common/PPCRentalAssertions.cs
+++ PPCRental.AcceptanceTest.Common/PPCRentalAssertions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PPCRental.Models;
+using FluentAssertion;
 
 namespace PPCRental.AcceptanceTest.Common
 {
@@ -17,5 +18,19 @@ namespace PPCRental.AcceptanceTest.Common
         {
             showViewListProject.Select(b => b.Email).Should().BeEquivalentTo(expectedEmail);
         }
+        public static void FoundPropertyShouldMatchTitles(IEnumerable<PROPERTY> foundBooks, IEnumerable<string> expectedTitles)
+        {
+            foundBooks.Select(b => b.PropertyName).Should().BeEquivalentTo(expectedTitles);
+        }
+
+        public static void FoundBooksShouldMatchTitlesInOrder(IEnumerable<PROPERTY> foundBooks, IEnumerable<string> expectedTitles)
+        {
+            foundBooks.Select(b => b.PropertyName).Should().Equal(expectedTitles);
+        }
+        public static void HomeScreenShouldShowInOrder(IEnumerable<PROPERTY> shownBooks, IEnumerable<string> expectedTitles)
+        {
+            shownBooks.Select(b => b.PropertyName).Should().Equal(expectedTitles);
+        }
+
     }
 }
---
+++ PPCRental.Appceptance.Test/StepDifinition/FilterProjectStep.cs
@@ -0,0 +1,35 @@
+﻿using TechTalk.SpecFlow;
+using PPCRental.AcceptanceTests.Driver.Search;
+
+namespace PPCRental.Appceptance.Test.StepDifinition
+{
+    [Binding, Scope(Tag = "automated")]
+    public class FilterProjectStep
+    {
+        private readonly SearchPropertyDriver _searchDriver;
+
+        public FilterProjectStep(SearchPropertyDriver driver)
+        {
+            _searchDriver = driver;
+        }
+
+        [When(@"I search for Property by the phrase '(.*)'")]
+        public void WhenISearchForPropertyByThePhrase(string searchText)
+        {
+            _searchDriver.Search(searchText);
+        }
+
+        [Then(@"the list of found Property should contain only: (.*)")]
+        public void ThenTheListOfFoundPropertyShouldContainOnly(string expectedTitleList)
+        {
+            _searchDriver.ShowProject(expectedTitleList);
+        }
+
+        [Then(@"the list of found Property should be:")]
+        public void ThenTheListOfFoundPropertyShouldBe(Table expectedProperty)
+        {
+            _searchDriver.ShowProject(expectedProperty);
+        }
+    }
+}
+
